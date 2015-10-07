bace2187d776eaa6c85ae35e60df69d734086532
Dylan Sharkey <Serghar12@gmail.com>
2015-10-06 17:47:00 -0700

    Added insertion sort

---
+++ insertion_sort.php
@@ -0,0 +1,27 @@
+<?php
+	function insertion($arr)
+	{
+		for ($i = 1; $i < count($arr); $i++)
+		{
+			$x = $arr[$i];
+			$y = $i;
+			while (($y > 0) && ($arr[$y - 1] > $x))
+			{
+				$arr[$y] = $arr[$y - 1];
+				$y--;
+			}
+			$arr[$y] = $x; 
+		}
+		return $arr;
+	}
+	
+	//just here 
+	$randArray = array();
+	for($x = 1; $x <= 10; $x++)
+	{
+		array_push($randArray, mt_rand(0, 10000));
+	}
+	var_dump($randArray);
+	var_dump(insertion($randArray));
+
+?>
