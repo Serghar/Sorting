b856a630735fbeb58eac7b76b24400cf7fc781d3
Dylan Sharkey <Serghar12@gmail.com>
2015-10-07 11:57:49 -0700

    Added max swap as well

--- selection.php
+++ selection.php
@@ -5,28 +5,64 @@
 		$runtime = 0; //Number of times a conditional statement is run
 
 		$start = microtime_float(); //Start time for timer
-		for($x = 0; $x < count($array); $x++)
-		{
-			$minValue = null;
-			$minKey = null;
-			for($y = $x; $y < count($array); $y++)
-			{
-				if($minValue == null || $minValue > $array[$y])
-				{
-					$minKey = $y;
-					$minValue = $array[$y];
-				}
-				$array[$minKey];
-				$runtime++;
-			}
-			$array[$minKey] = $array[$x];
-			$array[$x] = $minValue;
-		}
+		$top = (count($array) - 1);
+        for($x = 0; $x <= $top; $x++)
+        {
+        	//Find and swap the min value
+            $change = False;
+            $minValue = $array[$x];
+            $minIndex = $x;
+            for($y = $x; $y < count($array); $y++)
+            {
+                if($minValue > $array[$y])
+                {
+                    $minIndex = $y;
+                    $minValue = $array[$y];
+                    $change = True;
+                    $runtime++;
+                }
+            }
+            if($change)
+            {
+                swap($array, $x, $minIndex);
+                $change = False;
+                $runtime++;
+            }
+            
+            //Find and swap the max value
+            $maxValue = $array[$top];
+            $maxIndex = $top;
+            for($i = $top; $i > $x; $i--)
+            {
+                if($maxValue < $array[$i])
+                {
+                    $maxValue = $array[$i];
+                    $maxIndex = $i;
+                    $change = True;
+                    $runtime++;
+                }
+            }
+            if($change)
+            {
+                swap($array, $top, $maxIndex);
+                $change = False;
+                $runtime++;
+            }
+            $top--;
+        }
 		$end = microtime_float(); //End time for timer
 		$totalTime = $end - $start;
 		echo "This operation took " . $totalTime . " seconds to complete. ";
 		echo "It had ". $runtime . " if/else statements execute.";
 	}
+
+	//Take an array passed by reference and swaps 2 index position values 
+    function swap(&$arr, $x, $y)
+    {
+        $temp = $arr[$x];
+        $arr[$x] = $arr[$y];
+        $arr[$y] = $temp;
+    }
 
 
 	function microtime_float()
@@ -36,7 +72,7 @@
 	}
 
 	$randArray = array();
-	for($x = 1; $x <= 10000; $x++)
+	for($x = 1; $x <= 1000; $x++)
 	{
 		array_push($randArray, mt_rand(0, 10000));
 	}
